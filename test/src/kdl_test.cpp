118ec78c7f06731b4171544f2e7d4c827a9fd5a1
WinstonHeng <108784215+WinstonHeng@users.noreply.github.com>
2023-04-08 18:05:08 +0800

    Delete test directory

--- test/src/kdl_test.cpp
+++
@@ -1,7 +0,0 @@
-#include <kdl_parser/kdl_parser.hpp> 
- 
-KDL::Tree my_tree;
-if (!kdl_parser::treeFromFile("/home/winston/husky_ur5e_robot.urdf", my_tree)){
-      ROS_ERROR("Failed to construct kdl tree");
-      return false;
-   }
